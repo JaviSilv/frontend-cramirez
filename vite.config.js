fdea23c2b2d1fad76656adf98e6769867db65ef1
javier <jsilvab14@gmail.com>
2025-03-24 06:27:14 -0500

    vitaconfig

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+  server: {
+    host: true,
+    strictPort: true,
+  },
+  preview: {
+    allowedHosts: ["cramirezsac.onrender.com"], // Agrega tu dominio aquí
   },
 });
